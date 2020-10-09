459a84f99b7c6feb94fb662cf074cf7012dc24b7
Felipe Oliveira <58439493+felipeoliverai@users.noreply.github.com>
2020-10-09 15:06:25 -0300

    Create utils.py

---
+++ preprocessing/utils.py
@@ -0,0 +1,20 @@
+
+
+
+%%time 
+
+images = []
+labels = []
+data = []
+
+for dir in os.listdir(path):
+  for file_name in tqdm(os.listdir(os.path.join(path, dir))):
+
+    image_path = os.path.join(path, dir, file_name)
+    image = load_img(image_path, target_size=(150, 150, 3), color_mode="rgb")
+    image = img_to_array(image)
+    image = image.astype("float32")
+    image /= 255.0
+
+    images.append(image)
+    labels.append(dir)
