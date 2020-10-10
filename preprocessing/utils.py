d0cd13777f59c4b1e98d4d6601a5f1465e417fed
Felipe Oliveira <58439493+felipeoliverai@users.noreply.github.com>
2020-10-10 14:11:09 -0300

    Update utils.py

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -1,25 +1,35 @@
 import os 
 import pandas as pd 
 import numpy as np 
+import time
 from tqdm import tqdm
 from tensorflow.keras.preprocessing.image import load_img, array_to_img, img_to_array
 
 
+# load image dataset 
+def load_dataset(path): 
 
-%%time 
+  %%time 
 
-images = []
-labels = []
-data = []
+  images = []
+  labels = []
+  data = []
 
-for dir in os.listdir(path):
-  for file_name in tqdm(os.listdir(os.path.join(path, dir))):
+  for dir in os.listdir(path):
+    for file_name in tqdm(os.listdir(os.path.join(path, dir))):
 
-    image_path = os.path.join(path, dir, file_name)
-    image = load_img(image_path, target_size=(150, 150, 3), color_mode="rgb")
-    image = img_to_array(image)
-    image = image.astype("float32")
-    image /= 255.0
+      image_path = os.path.join(path, dir, file_name)
+      image = load_img(image_path, target_size=(150, 150, 3), color_mode="rgb")
+      image = img_to_array(image)
+      image = image.astype("float32")
+      image /= 255.0
 
-    images.append(image)
-    labels.append(dir)
+      images.append(image)
+      labels.append(dir)
+ 
+  images = np.array(images, dtype = 'float32')
+  labels = np.array(labels, dtype = 'int32')   
+        
+  data.append((images, labels))
+  
+  return data
